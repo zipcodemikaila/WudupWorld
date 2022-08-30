0e1613eeb4e6395af2be701903113fddbd6f351c
mikaila <mikaila@zipcodewilmington.com>
2022-08-30 15:44:15 -0400

    my first intelli code on github and i love it

---
+++ src/main/java/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    public static void main(String[] args) {
+        System.out.println("I got this shit");
+    }
+}
+
+//Create an intellij Maven project
+//Create a Java class filed named Person
+//Put a public static void main inside
+//Print out I got this shit!
